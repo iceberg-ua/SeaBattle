5e69072d4419f1a17fd371768c48ab16d40318c2
iceberg <wladimir.melnik@gmail.com>
2023-04-01 15:26:41 +0200

    inject battle state. clean up seacell component

--- Shared/BattleState.cs
+++ Shared/BattleState.cs
@@ -5,7 +5,7 @@ public enum CellState { empty = 0, hit, miss, ship }
 public class BattleState
 {
     private int _size = 10;
-    private int[,] _battleField;
+    private int[,] _ownFleet;
 
     public BattleState()
     {
@@ -14,8 +14,20 @@ public class BattleState
 
     public bool InProgress { get; set; } = false;
 
-    public void InitBattleField(int size)
+    public int Size
     {
-        _battleField = new int[size, size];
+        get { return _size; }
+        set
+        {
+            _size = value;
+            InitBattleField(_size);
+        }
     }
+
+    public int[,] OwnFleet
+    {
+        get => _ownFleet;
+    }
+
+    public void InitBattleField(int size) => _ownFleet = new int[size, size];
 }
