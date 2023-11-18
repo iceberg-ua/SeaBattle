be2817633c718b92bb1c06896b454618afa89bde
iceberg <wladimir.melnik@gmail.com>
2023-11-18 22:01:02 +0100

    create and return correct game state

--- Shared/GlobalStorage.cs
+++ Shared/GlobalStorage.cs
@@ -3,12 +3,25 @@
 public class GlobalStorage
 {
     private readonly Dictionary<Guid, GameState> _gamesStorage = new();
+    private GameState? _vacantGame = null;
 
     public GameState GetGame(Guid gameId)
     {
-        if(!_gamesStorage.TryGetValue(gameId, out var state))
-            state = new GameState();
+        if (!_gamesStorage.TryGetValue(gameId, out var game))
+        {
+            if (_vacantGame != null)
+            {
+                game = _vacantGame;
+                _vacantGame = null;
+            }
+            else
+            {
+                game = new GameState();
+                _gamesStorage.Add(game.ID, game);
+                _vacantGame = game;
+            }
+        }
 
-        return state;
+        return game;
     }
 }
