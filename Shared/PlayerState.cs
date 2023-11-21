b24f2121da0e68e7c691db35a8798b22ce2f2743
iceberg <wladimir.melnik@gmail.com>
2023-11-21 23:00:11 +0100

    restrict diagonal placement

--- Shared/PlayerState.cs
+++ Shared/PlayerState.cs
@@ -31,14 +31,48 @@ public class PlayerState
 
     #endregion
 
+    private CellState GetCell(int x, int y) => Armada[x * 10 + y];
+
+    private void SetCell(int x, int y, CellState value) => Armada[x * 10 + y] = value;
+
     public bool TryToUpdateState(int x, int y)
     {
         if (!InProgress)
         {
-            var value = Armada[x * 10 + y] == CellState.ship ? CellState.empty : CellState.ship;
-            Armada[x * 10 + y] = value;
+            if (GetCell(x, y) == CellState.ship)
+                SetCell(x, y, CellState.empty);
+            else if (CheckFormation(x, y))
+                SetCell(x, y, CellState.ship);
+            else
+                return false;
+
         }
 
         return true;
     }
+
+    private bool CheckFormation(int x, int y)
+    {
+        if (OnDiagonal(x, y))
+            return false;
+
+        SetCell(x, y, CellState.ship);
+
+        for (int i = 0; i < FieldSize; i++)
+        {
+            for (int j = 0; j < FieldSize; j++)
+            {
+                //if( GetCell(x, y) == CellState.Deck)
+                //    CheckShip(i, j);
+            }
+        }
+
+        return true;
+    }
+
+    private bool OnDiagonal(int x, int y) => CellIsOccupied(x - 1, y - 1) || CellIsOccupied(x + 1, y - 1) ||
+                                             CellIsOccupied(x - 1, y + 1) || CellIsOccupied(x + 1, y + 1);
+
+    private bool CellIsOccupied(int x, int y) =>  x >= 0 && x < FieldSize &&
+                                                  y >= 0 && y < FieldSize && GetCell(x, y) == CellState.ship;
 }
