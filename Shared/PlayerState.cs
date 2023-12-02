2967d8922b33a62fc71f2b02c24915f537e1eff5
iceberg <wladimir.melnik@gmail.com>
2023-12-02 20:37:45 +0100

    add ship to a ships list. first attempt

--- Shared/PlayerState.cs
+++ Shared/PlayerState.cs
@@ -31,21 +31,81 @@ public class PlayerState
 
     #endregion
 
+    private readonly int _maxShipSize = 4;
+    private readonly Dictionary<int, int> _shipCounts = new() { { 1, 4 }, { 2, 3 }, { 3, 2 }, { 4, 1 } };
+    private readonly List<List<(int x, int y)>> _ships = new(10);
+    private bool _full = false;
+
     private CellState GetCell(int x, int y) => Armada[x * 10 + y];
 
     private void SetCell(int x, int y, CellState value) => Armada[x * 10 + y] = value;
+
+    
+    /// <summary>
+    /// Update the ships on the battlefiled
+    /// </summary>
+    /// <param name="x"></param>
+    /// <param name="y"></param>
+    /// <returns>true - if legal, false - if not</returns>
+    private bool UpdateFormation(int x, int y)
+    {
+        if (_full || OnDiagonal(x, y))
+            return false;
+
+        var existingShips = _ships.Where(ship => ship.Contains((x - 1, y)) ||
+                                                 ship.Contains((x + 1, y)) ||
+                                                 ship.Contains((x, y - 1)) ||
+                                                 ship.Contains((x, y + 1)));
+
+        if(existingShips.Count() > 1)
+        {
+            if(existingShips.Sum(s => s.Count) + 1 < 4)
+            {
+                _ships.RemoveAll(s => existingShips.Contains(s));
+
+                var newShip = existingShips.SelectMany(s => s).ToList(); 
+            }
+        }
+
+        _full = CheckShipsCount();
+        return true;
+    }
+
+    private bool CheckShipsCount()
+    {
+        var shipGroups = _ships.Select(s => s.Count).GroupBy(s => s).OrderBy(g => g.Key);
+        bool full = true;
+       
+        foreach (var group in shipGroups)
+        {
+            if (_shipCounts[group.Key] == group.Count())
+                full &= true;
+            else if (_shipCounts[group.Key] < group.Count())
+                full &= false;
+            else
+                throw new ArgumentOutOfRangeException("There is more ships then allowed");
+        }
+
+        return full;
+    }
+
+    private bool OnDiagonal(int x, int y) => CellIsOccupied(x - 1, y - 1) || CellIsOccupied(x + 1, y - 1) ||
+                                             CellIsOccupied(x - 1, y + 1) || CellIsOccupied(x + 1, y + 1);
+
+    private bool CellIsOccupied(int x, int y) =>  x >= 0 && x < FieldSize &&
+                                                  y >= 0 && y < FieldSize && GetCell(x, y) == CellState.ship;
 
     public bool TryToUpdateState(int x, int y)
     {
         if (!InProgress)
         {
-            if (GetCell(x, y) == CellState.ship)
+            if (CellIsOccupied(x, y))
             {
                 var ship = _ships.Where(s => s.Contains((x, y))).FirstOrDefault();
                 ship?.Remove((x, y));
                 SetCell(x, y, CellState.empty);
             }
-            else if (CheckFormation(x, y))
+            else if (UpdateFormation(x, y))
                 SetCell(x, y, CellState.ship);
             else
                 return false;
@@ -54,44 +114,4 @@ public class PlayerState
 
         return false;
     }
-
-    private readonly int _maxShipSize = 4;
-    private readonly Dictionary<int, int> _shipCounts = new() { { 1, 4 }, { 2, 3 }, { 3, 2 }, { 4, 1 } };
-    private readonly List<List<(int x, int y)>> _ships = new(10);
-    private bool _full = false;
-
-    private bool CheckFormation(int x, int y)
-    {
-        if (_full || OnDiagonal(x, y))
-            return false;
-
-        SetCell(x, y, CellState.ship);
-
-        return true;
-    }
-
-    private bool CheckShipCount()
-    {
-        var shipGroups = _ships.Select(s => s.Count).GroupBy(s => s).OrderBy(g => g.Key);
-        bool full = false;
-       
-        foreach (var group in shipGroups)
-        {
-            if (_shipCounts[group.Key] == group.Count())
-                full = true;
-            else if (_shipCounts[group.Key] < group.Count())
-                full = false;
-            else
-                return false;
-        }
-
-        _full = full;
-        return true;
-    }
-
-    private bool OnDiagonal(int x, int y) => CellIsOccupied(x - 1, y - 1) || CellIsOccupied(x + 1, y - 1) ||
-                                             CellIsOccupied(x - 1, y + 1) || CellIsOccupied(x + 1, y + 1);
-
-    private bool CellIsOccupied(int x, int y) =>  x >= 0 && x < FieldSize &&
-                                                  y >= 0 && y < FieldSize && GetCell(x, y) == CellState.ship;
 }
