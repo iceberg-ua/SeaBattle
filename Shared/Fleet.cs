8cddc678b6e97edf4a601e4bfd6dcaaa35803dac
iceberg <wladimir.melnik@gmail.com>
2024-01-21 15:40:01 +0100

    add model for fleet and ship. implement adding and removing deck on the field

---
+++ Shared/Fleet.cs
@@ -0,0 +1,79 @@
+﻿namespace SeaBattle.Shared;
+
+public class Fleet
+{
+    private readonly int _maxShipSize = 4;
+
+    public List<Ship> Ships { get; set; } = [];
+
+    public bool Complete { get; private set; }
+
+    public bool AddShipDeck(int x, int y)
+    {
+        if (Complete)
+            return false;
+
+        var existingShips = Ships.Where(ship => ship.Contains((x - 1, y)) ||
+                                                ship.Contains((x + 1, y)) ||
+                                                ship.Contains((x, y - 1)) ||
+                                                ship.Contains((x, y + 1))).ToList();
+        var count = existingShips.Count;
+
+        if (count > 1)
+        {
+            if (existingShips.Sum(s => s.Size) + 1 > _maxShipSize)
+                return false;
+
+            Ships.RemoveAll(existingShips.Contains);
+            var newShip = new Ship();
+
+            foreach (var item in existingShips)
+            {
+                newShip.AddRange(item);
+            }
+
+            newShip.AddDeck((x, y));
+            Ships.Add(newShip);
+        }
+        else if (count == 1)
+        {
+            var ship = existingShips.First();
+
+            if (ship.Size == _maxShipSize)
+                return false;
+
+            ship.AddDeck((x, y));
+        }
+        else
+            Ships.Add([(x, y)]);
+
+        return true;
+    }
+
+    public void RemoveShipDeck(int x, int y)
+    {
+        var existingShips = Ships.Where(s => s.Contains((x, y)));
+
+        if (!existingShips.Any())
+            return;
+
+        var owner = existingShips.First();
+        var index = owner.IndexOf((x, y));
+
+        var firstPart = owner[..index];
+        var secondPart = owner[(index + 1)..];
+
+        Ships.Remove(owner);
+
+        if (firstPart.Count > 0)
+            Ships.Add(new Ship(firstPart));
+
+        if (secondPart.Count > 0)
+            Ships.Add(new Ship(secondPart));
+    }
+
+    public void Clear()
+    {
+        Ships.Clear();
+    }
+}
